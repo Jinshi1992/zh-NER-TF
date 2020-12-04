5b0334324b8e1e26d06fdfa5c3f8cbef58b854d5
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2020-12-04 03:21:25 +0100

    Update build_data.py

--- build_data.py
+++ build_data.py
@@ -16,35 +16,39 @@ def main():
         config: (instance of Config) has attributes like hyper-params...
     """
     # get config and processing of words
-    config = Config(load=False)
+    #config = Config(load=False)
     processing_word = get_processing_word(lowercase=True)
-
+    
     # Generators
-    dev   = CoNLLDataset(config.filename_dev, processing_word)
-    test  = CoNLLDataset(config.filename_test, processing_word)
-    train = CoNLLDataset(config.filename_train, processing_word)
+    dev   = CoNLLDataset("data_path/test_data", processing_word)
+    test  = CoNLLDataset("data_path/test_data", processing_word)
+    train = CoNLLDataset("data_path/train_data", processing_word)
 
     # Build Word and Tag vocab
     vocab_words, vocab_tags = get_vocabs([train, dev, test])
-    vocab_glove = get_glove_vocab(config.filename_glove)
+    #vocab_glove = get_glove_vocab(config.filename_glove)
 
-    vocab = vocab_words & vocab_glove
+    vocab = vocab_words #& vocab_glove
     vocab.add(UNK)
     vocab.add(NUM)
-
+    
+    filename_words = "data/words.txt"
+    filename_tags = "data/tags.txt"
+    filename_chars = "data/chars.txt"
+    
     # Save vocab
-    write_vocab(vocab, config.filename_words)
-    write_vocab(vocab_tags, config.filename_tags)
+    write_vocab(vocab, filename_words)
+    write_vocab(vocab_tags, filename_tags)
 
     # Trim GloVe Vectors
-    vocab = load_vocab(config.filename_words)
-    export_trimmed_glove_vectors(vocab, config.filename_glove,
+    vocab = load_vocab(filename_words)
+    #export_trimmed_glove_vectors(vocab, config.filename_glove,
                                 config.filename_trimmed, config.dim_word)
 
     # Build and save char vocab
-    train = CoNLLDataset(config.filename_train)
+    train = CoNLLDataset("data_path/train_data")
     vocab_chars = get_char_vocab(train)
-    write_vocab(vocab_chars, config.filename_chars)
+    write_vocab(vocab_chars, filename_chars)
 
 
 if __name__ == "__main__":
