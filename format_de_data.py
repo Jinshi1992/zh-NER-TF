452e33569b660033a24c3eff1430b60d36bced76
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2020-12-07 06:43:41 +0100

    Create format_de_data.py

---
+++ format_de_data.py
@@ -0,0 +1,29 @@
+import sys, pickle, os, random
+import numpy as np
+
+def main():
+
+    read corpus and return the list of samples
+    :param corpus_path:
+    :return: data
+    """
+    
+    corpus_path = "data_path/train_data"
+    
+    data = []
+    with open(corpus_path, encoding='utf-8') as fr:
+        lines = fr.readlines()
+    sent_, tag_ = [], []
+    for line in lines:
+        if line != '\n':
+            char = line.strip().split(' ')[0]
+            label = line.strip().split(' ')[-1]
+            sent_.append(char)
+            tag_.append(label)
+        else:
+            data.append((sent_, tag_))
+            sent_, tag_ = [], []
+        with open("train_data_format.txt","w") as f:
+            for (sent_, tag_) in data:
+                f.write(sent_, tag_)
+    
