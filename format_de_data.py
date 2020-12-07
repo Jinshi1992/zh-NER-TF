7eb9b3ddaedef9201c88aa21ca8fbf9c9a49da64
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2020-12-07 08:54:42 +0100

    Update format_de_data.py

--- format_de_data.py
+++ format_de_data.py
@@ -24,7 +24,11 @@ def main():
             data.append(line)
 
     with open('data_path/train_data_format.txt', 'w') as fp:
-        fp.write('\n'.join('{} {}'.format(x[0],x[1]) for x in data))
+        for i in data:
+            if i != '\n':
+                fp.write('\n'.join('{} {}'.format(x[0],x[1]) for x in data))
+            else:
+                fp.write('\n')
         fp.close()
     
     
