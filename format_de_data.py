5b3cda37560cc169abe67cb6160303e43651c553
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2020-12-07 07:57:39 +0100

    Update format_de_data.py

--- format_de_data.py
+++ format_de_data.py
@@ -25,5 +25,7 @@ def main():
     with open("train_data_format.txt","w") as f:
         f.write('\n'.join('{} {}'.format(x[0],x[1]) for x in data)
                 
+     
+                
 if __name__ == "__main__":
     main()
