100362f8acc16d1444e81b4af8d36f70a0b48b1e
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2020-12-10 05:03:56 +0100

    Update build_vocab_test.py

--- build_vocab_test.py
+++ build_vocab_test.py
@@ -44,14 +44,21 @@ def main():
     
     for sent_, tag_ in data:
         for word in sent_:
-            print(word)
             if word.isdigit():
                 word = '<NUM>'
+            elif ('\u0041' <= word <='\u005a') or ('\u0061' <= word <='\u007a'):
+                word = '<ENG>'
             if word not in word2id:
                 word2id[word] = [len(word2id)+1, 1]
             else:
                 word2id[word][1] += 1
-     
+    low_freq_words = []
+    for word, [word_id, word_freq] in word2id.items():
+        if word_freq < min_count and word != '<NUM>' and word != '<ENG>':
+            low_freq_words.append(word)
+    for word in low_freq_words:
+        del word2id[word]
+
     new_id = 1
     for word in word2id.keys():
         word2id[word] = new_id
