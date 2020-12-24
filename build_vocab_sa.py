8ac613065320748f4dc43955ff45729bce54dacd
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2020-12-24 04:24:47 +0100

    Create build_vocab_sa.py

---
+++ build_vocab_sa.py
@@ -0,0 +1,73 @@
+import sys, pickle, os, random
+import numpy as np
+
+def read_corpus(corpus_path):
+    """
+    read corpus and return the list of samples
+    :param corpus_path:
+    :return: data
+    """
+    data = []
+    with open(corpus_path, encoding='utf-8') as fr:
+        lines = fr.readlines()
+    sent_, tag_ = [], []
+    for line in lines:
+        if line != '\n':
+            [char, label] = line.strip().split()
+            sent_.append(char)
+            tag_.append(label)
+        else:
+            data.append((sent_, tag_))
+            sent_, tag_ = [], []
+
+    return data
+    
+def main():
+   
+    vocab_path = "data_path/word2id_zh_2.pkl"
+    
+    train_path = "data_path/train_data"
+    test_path = "data_path/test_data"
+    #dev_path = "data_path/dev_data"
+    
+    min_count = 3
+    
+    data_train = read_corpus(train_path)
+    #data_dev = read_corpus(dev_path)
+    data_test = read_corpus(test_path)
+    
+    data = data_train + data_test
+    word2id = {}
+    
+    for sent_, tag_ in data:
+        for word in sent_:
+            if word.isdigit():
+                word = '<NUM>'
+            elif ('\u0041' <= word <='\u005a') or ('\u0061' <= word <='\u007a'):
+                word = '<ENG>'
+            if word not in word2id:
+                word2id[word] = [len(word2id)+1, 1]
+            else:
+                word2id[word][1] += 1
+    low_freq_words = []
+    for word, [word_id, word_freq] in word2id.items():
+        if word_freq < min_count and word != '<NUM>' and word != '<ENG>':
+            low_freq_words.append(word)
+    for word in low_freq_words:
+        del word2id[word]
+
+    new_id = 1
+    for word in word2id.keys():
+        word2id[word] = new_id
+        new_id += 1
+    word2id['<UNK>'] = new_id
+    word2id['<PAD>'] = 0
+
+    print(len(word2id))
+    with open(vocab_path, 'wb') as fw:
+        pickle.dump(word2id, fw)
+
+   
+   
+if __name__ == "__main__":
+    main()
