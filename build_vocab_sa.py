fc7663d95ab64311fbfcd17f09ae963e42729efc
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2021-01-02 05:00:41 +0100

    Update build_vocab_sa.py

--- build_vocab_sa.py
+++ build_vocab_sa.py
@@ -27,31 +27,33 @@ def read_corpus(corpus_path):
     
 def main():
    
-    vocab_path = "data_path/word2id_zh_2.pkl"
+    vocab_path = "data_path/word2id_sa_en.pkl"
     
-    train_path = "data_path/train_data"
-    test_path = "data_path/test_data"
+    test_sa_path = "data_path/pkl_test"
+    #train_path = "data_path/train_data"
+    #test_path = "data_path/test_data"
     #dev_path = "data_path/dev_data"
     
     min_count = 3
     
-    data_train = read_corpus(train_path)
+    data_sa_test = read_corpus(test_sa_path)
+    #data_train = read_corpus(train_path)
     #data_dev = read_corpus(dev_path)
-    data_test = read_corpus(test_path)
+    #data_test = read_corpus(test_path)
     
-    data = data_train + data_test
+    #data = data_train + data_test
+    data = data_sa_test
     word2id = {}
     
-    for sent_, tag_ in data:
-        for word in sent_:
+    for words in data:
+        for word in words:
             if word.isdigit():
                 word = '<NUM>'
-            elif ('\u0041' <= word <='\u005a') or ('\u0061' <= word <='\u007a'):
-                word = '<ENG>'
             if word not in word2id:
                 word2id[word] = [len(word2id)+1, 1]
             else:
                 word2id[word][1] += 1
+                
     low_freq_words = []
     for word, [word_id, word_freq] in word2id.items():
         if word_freq < min_count and word != '<NUM>' and word != '<ENG>':
