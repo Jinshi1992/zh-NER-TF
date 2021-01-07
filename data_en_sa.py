d91ff63a2d3ff950c192d10379920114a1d32877
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2021-01-07 11:25:15 +0100

    Update data_en_sa.py

--- data_en_sa.py
+++ data_en_sa.py
@@ -140,7 +140,7 @@ def batch_yield(data, batch_size, vocab, tag2label, shuffle=False):
             seqs, labels = [], []
 
         seqs.append(sentid)
-        labels.append(label[0])
+        labels.append(label)
 
     if len(seqs) != 0:
         yield seqs, labels
