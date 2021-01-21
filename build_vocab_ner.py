27e35f5a25fda6e8a17e45ffff3243160e4f4d67
Jinshi Wang <46356641+Jinshi1992@users.noreply.github.com>
2021-01-21 23:42:16 +0100

    Update build_vocab_ner.py

--- build_vocab_ner.py
+++ build_vocab_ner.py
@@ -13,27 +13,27 @@ def read_corpus(corpus_path):
     sent_, tag_ = [], []
     for line in lines:
         if line != '\n':
-            [char, label] = line.strip().split()
+            char = line.strip().split(' ')[0]
+            label = line.strip().split(' ')[-1]
             sent_.append(char)
             tag_.append(label)
         else:
             data.append((sent_, tag_))
             sent_, tag_ = [], []
-
     return data
     
 def main():
    
-    vocab_path = "data_path/word2id_zh_2.pkl"
+    vocab_path = "data_path/word2id_de.pkl"
     
     train_path = "data_path/train_data"
     test_path = "data_path/test_data"
-    #dev_path = "data_path/dev_data"
+    dev_path = "data_path/dev_data"
     
     min_count = 3
     
     data_train = read_corpus(train_path)
-    #data_dev = read_corpus(dev_path)
+    data_dev = read_corpus(dev_path)
     data_test = read_corpus(test_path)
     
     data = data_train + data_test
@@ -43,8 +43,6 @@ def main():
         for word in sent_:
             if word.isdigit():
                 word = '<NUM>'
-            elif ('\u0041' <= word <='\u005a') or ('\u0061' <= word <='\u007a'):
-                word = '<ENG>'
             if word not in word2id:
                 word2id[word] = [len(word2id)+1, 1]
             else:
